ea3d3cb5a6f7039d5906ea719179cf730499d695
Simon Heath <icefoxen@gmail.com>
2016-03-26 18:39:31 -0500

    We successfully reverse a string and send it back to the client!
    What more do we need?

--- muud/main.fs
+++ muud/main.fs
@@ -3,6 +3,23 @@ module Muud
 open System.Net
 open System.Net.Sockets
 
+let handleClient (client : TcpClient) =
+  let stream = client.GetStream()
+  let bufflen = 256
+  let bytes : byte array = Array.zeroCreate bufflen
+  let rec loop () =
+    let i = stream.Read(bytes, 0, bufflen)
+    if i = 0 then
+      ()
+    else
+      let str = System.Text.Encoding.ASCII.GetString(bytes, 0, i)
+      printf "Got %s\n" str
+      let returnMessage = Array.rev bytes.[0..i-1]
+      stream.Write(returnMessage, 0, i)
+      printf "Wrote message back: '%A'\n" returnMessage
+  loop ()
+
+
 let listen () =
   let address = IPAddress.Any
   let port = 9999
@@ -13,6 +30,7 @@ let listen () =
     printf "Waiting for connection\n"
     let client = listener.AcceptTcpClient ()
     printf "Got connection\n"
+    handleClient client
 
   loop ()
 
