bea67cdc712767805e409658ab3dec448f702913
Simon Heath <icefoxen@gmail.com>
2016-03-26 19:42:40 -0500

    I think I'm too sleepy for this right now.

--- muud/main.fs
+++ muud/main.fs
@@ -3,7 +3,20 @@ module Muud
 open System.Net
 open System.Net.Sockets
 
+
 let handleClient (client : TcpClient) =
+  let stream = client.GetStream()
+  let bufflen = 256
+  let bytes : byte array = Array.zeroCreate bufflen
+  let i = stream.Read(bytes, 0, bufflen)
+  if i = 0 then
+    ()
+  else
+    let str = System.Text.Encoding.ASCII.GetString(bytes, 0, i)
+    printf "Got %s\n" str
+    let returnMessage = Array.rev bytes.[0..i-1]
+    stream.Write(returnMessage, 0, i)
+(*
   let stream = client.GetStream()
   let bufflen = 256
   let bytes : byte array = Array.zeroCreate bufflen
@@ -20,14 +33,10 @@ let handleClient (client : TcpClient) =
       loop () |> Async.RunSynchronously
     }
   loop ()
+*)
 
 
-let listen () =
-  let address = IPAddress.Any
-  let port = 9999
-  let listener = new TcpListener(address, port)
-  listener.Start()
-
+(*
   let rec loop () =
     printf "Waiting for connection\n"
     let client = listener.AcceptTcpClient ()
@@ -36,10 +45,27 @@ let listen () =
     loop ()
 
   loop ()
+  *)
 
+let rec serverLoop (listener:TcpListener) =
+  printf "Waiting for connection\n"
+  async {
+    let client = listener.AcceptTcpClient ()
+    printf "Got connection\n"
+    handleClient client
+  }
+  serverLoop listener
+
+
+let listen () =
+  let address = IPAddress.Any
+  let port = 9999
+  let listener = new TcpListener(address, port)
+  listener.Start()
+  serverLoop listener
 
 let main () =
   printf "Hello world!\n"
-  listen () |> Async.RunSynchronously
+  listen ()
 
 main ()
